1850cfe831396938a8e8f7411e9d4681667cdcad
Shayna Snyder <ssnyder19@my.whitworth.edu>
2016-11-15 12:58:24 -0800

    Initial Commit

---
+++ CS172-Final Project/CS172-Final Project/main.cpp
@@ -0,0 +1,17 @@
+//Cheree LaPierre and Shayna Snyder
+//CS172-1
+//Final Project
+//Text-Adventure Battleship
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
