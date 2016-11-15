c83f3d6b4163958bb96537d9425ff972b9539ce0
Shayna Snyder <ssnyder19@my.whitworth.edu>
2016-11-15 13:29:05 -0800

    Workin

--- CS172-Final Project/CS172-Final Project/main.cpp
+++ CS172-Final Project/CS172-Final Project/main.cpp
@@ -13,18 +13,17 @@ using namespace std;
 int main()
 {
     //Patrol Boat-size 2, Destroyer-size 3, Submarine-size 3, Battleship-size 4, Carrier-size 5
-    string ships[5] = {"Patrol Boat", "Destroyer", "Submarine", "Battleship", "Carrier"};
+    //declares string array to hold ship names
+    string shipNames[5] = {"Patrol Boat", "Destroyer", "Submarine", "Battleship", "Carrier"};
+    //declares int arrays to hold ship sizes and locations
     int shipSize[5] = {2, 3, 3, 4, 5};
     int location[5];
     cout << "Background information and rules.\n";
     //prompts user to place their ships using for loop
     for(int i=0; i<5; i++)
     {
-        cout << "Place your " << ships[i] << ": ";
+        cout << "Place your " << shipNames[i] << ": ";
         cin >> location[i];
     }
-
-    cout << "Hello Cheree";
-
     return 0;
 }
