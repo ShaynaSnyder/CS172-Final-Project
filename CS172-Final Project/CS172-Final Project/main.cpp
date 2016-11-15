345fbcf812c1a3085a180a67248f5be04bfd099c
Shayna Snyder <ssnyder19@my.whitworth.edu>
2016-11-15 13:12:07 -0800

    BATTLESHIP

--- CS172-Final Project/CS172-Final Project/main.cpp
+++ CS172-Final Project/CS172-Final Project/main.cpp
@@ -12,6 +12,14 @@ using namespace std;
 
 int main()
 {
-    
+    //Patrol Boat-size 2, Destroyer-size 3, Submarine-size 3, Battleship-size 4, Carrier-size 5
+    string ships[5] = {"Patrol Boat", "Destroyer", "Submarine", "Battleship", "Carrier"};
+    int location[5];
+    cout << "Background information and rules.\n";
+    for(int i=0; i<5; i++)
+    {
+        cout << "Place your " << ships[i] << ": ";
+        cin >> location[i];
+    }
     return 0;
 }
