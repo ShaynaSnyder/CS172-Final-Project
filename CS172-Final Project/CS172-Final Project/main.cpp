60d98c2865b3c7e6b0f9597b1ce40667408411f6
Shayna Snyder <ssnyder19@my.whitworth.edu>
2016-12-06 12:54:58 -0800

    moved function definitions to after int main()

--- CS172-Final Project/CS172-Final Project/main.cpp
+++ CS172-Final Project/CS172-Final Project/main.cpp
@@ -14,95 +14,10 @@ const int empty    = 0;  // empty
 const int occupied = 1;  // contains a ship
 const int missed   = 2;  // missed
 const int hit      = 3;  // hit
-
-void playerBoard(int array1[5][5]) //function initializes gameboard for player
-{
-    // create a blank board
-    for (int x = 0; x <= 4; x++)
-    {
-        for (int y = 0; y <= 4; y++)
-        {
-            array1[x][y] = empty;
-        }
-    }
-}
-void computerBoard(int array1[5][5]) //function initializes gameboard for computer
-{
-    // create a blank board
-    for (int x = 0; x <= 4; x++)
-    {
-        for (int y = 0; y <= 4; y++)
-        {
-            array1[x][y] = empty;
-        }
-    }
-}
-
-void printPlayerBoard(int array2[5][5])         //function outputs the player's board
-{
-    cout << "\n  ";
-    for(int i = 1; i <= 5; i++)                     //outputs column names
-    { //number coordinates
-        cout << setw(4) << i;
-    }
-    cout << "\n" << "   _____________________" << endl;
-    for(int a = 0; a <= 4; a++)
-    { //letter coordinates
-        cout << " " << (char) (a + 65) << " |";   //outputs row names
-        for(int j = 0; j <= 4; j++)
-        {
-            
-            if(array2[j][a] == occupied)        //shows that your ship is occupying this space
-            {
-                cout << setw(4) << "X |";
-            }
-            if(array2[j][a] == empty)           //leaves this space empty
-            {
-                cout << setw(4) << " |" ;
-            }
-            else if(array2[j][a] == missed )    //shows if computer misses your ship
-            {
-                cout << setw(4) << "M |";
-            }
-            else if(array2[j][a] == hit )       //shows if computer hits your ship
-            {
-                cout << setw(4) << "H |";
-            }
-        }
-        cout << "\n" << "   _____________________" << endl;
-    }
-}
-
-void printComputerBoard(int array2[5][5])
-{
-    cout << "  ";
-    for(int i = 1; i <= 5; i++)                     //outputs column names
-    { //number coordinates
-        cout << setw(4) << i;
-    }
-    cout << "\n" << "   _____________________" << endl;
-    for(int a = 0; a <= 4; a++)
-    { //letter coordinates
-        cout << " " << (char) (a + 65) << " |";   //outputs row names
-        for(int j = 0; j <= 4; j++)
-        {
-            
-            if((array2[j][a] == occupied) || (array2[j][a] == empty))   //space appears blank (could be empty or occupied)
-            {
-                cout << setw(4) << " |" ;
-            }
-            else if(array2[j][a] == missed )    //shows your missed shots
-            {
-                cout << setw(4) << "M |";
-            }
-            else if(array2[j][a] == hit )       //shows your hits
-            {
-                cout << setw(4) << "H |";
-            }
-        }
-        cout << "\n" << "   _____________________" << endl;
-    }
-}
+void playerBoard(int array1[5][5]); //declares void function to create the player's board
+void computerBoard(int array1[5][5]); //declares void function to create the computer's board
+void printPlayerBoard(int array2[5][5]); //declares void function to send the player's board to the screen
+void printComputerBoard(int array2[5][5]); //declares void function to send the computer's board to the screen
 
 int main()
 {
@@ -113,10 +28,9 @@ int main()
     int xlocation[5], ylocation[5], board[5][5], repeat=0;
     char letterlocation[5];
     cout << "\nBackground information and rules.\n";
-    playerBoard(board);
-    printPlayerBoard(board);
+    playerBoard(board); //calls void function that creates the player's board
+    printPlayerBoard(board); //calls void function that outputs the player's board
     //prompts user to place their ships using for loop
-    
     for(int i=0; i<=4; i++)
     {
         do
@@ -160,10 +74,96 @@ int main()
     }while(repeat==1);
         board[xlocation[i]-1][ylocation[i]]=occupied;
     }
-    printPlayerBoard(board);
-
-    
-    
-   
+    printPlayerBoard(board); //calls void function printPlayerBoard
     return 0;
+}
+//defines the function that creates the player's board
+void playerBoard(int array1[5][5])
+{
+    // create a blank board
+    for (int x = 0; x <= 4; x++)
+    {
+        for (int y = 0; y <= 4; y++)
+        {
+            array1[x][y] = empty;
+        }
+    }
+}
+//defines the function that creates the computer's board
+void computerBoard(int array1[5][5])
+{
+    // create a blank board
+    for (int x = 0; x <= 4; x++)
+    {
+        for (int y = 0; y <= 4; y++)
+        {
+            array1[x][y] = empty;
+        }
+    }
+}
+//defines the function that sends the player's board to the screen
+void printPlayerBoard(int array2[5][5])
+{
+    cout << "\n  ";
+    for(int i = 1; i <= 5; i++)                     //outputs column names
+    { //number coordinates
+        cout << setw(4) << i;
+    }
+    cout << "\n" << "   _____________________" << endl;
+    for(int a = 0; a <= 4; a++)
+    { //letter coordinates
+        cout << " " << (char) (a + 65) << " |";   //outputs row names
+        for(int j = 0; j <= 4; j++)
+        {
+            
+            if(array2[j][a] == occupied)        //shows that your ship is occupying this space
+            {
+                cout << setw(4) << "X |";
+            }
+            if(array2[j][a] == empty)           //leaves this space empty
+            {
+                cout << setw(4) << " |" ;
+            }
+            else if(array2[j][a] == missed )    //shows if computer misses your ship
+            {
+                cout << setw(4) << "M |";
+            }
+            else if(array2[j][a] == hit )       //shows if computer hits your ship
+            {
+                cout << setw(4) << "H |";
+            }
+        }
+        cout << "\n" << "   _____________________" << endl;
+    }
+}
+//defines function that sends the computer's board to the screen
+void printComputerBoard(int array2[5][5])
+{
+    cout << "  ";
+    for(int i = 1; i <= 5; i++)                     //outputs column names
+    { //number coordinates
+        cout << setw(4) << i;
+    }
+    cout << "\n" << "   _____________________" << endl;
+    for(int a = 0; a <= 4; a++)
+    { //letter coordinates
+        cout << " " << (char) (a + 65) << " |";   //outputs row names
+        for(int j = 0; j <= 4; j++)
+        {
+            
+            if((array2[j][a] == occupied) || (array2[j][a] == empty))   //space appears blank (could be empty or occupied)
+            {
+                cout << setw(4) << " |" ;
+            }
+            else if(array2[j][a] == missed )    //shows your missed shots
+            {
+                cout << setw(4) << "M |";
+            }
+            else if(array2[j][a] == hit )       //shows your hits
+            {
+                cout << setw(4) << "H |";
+            }
+        }
+        cout << "\n" << "   _____________________" << endl;
+    }
 }
